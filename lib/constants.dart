0e9f54df30967651f680cb64e97625c3cae72ddd
takizee <takizee53@gmail.com>
2022-02-28 17:03:56 +0700

    :lipstick: Changing the color theme of the app

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+const salahConcreteColor= Color(0xFF393936);
+const salahSandColor= Color(0xFFc2b280);
+const nagyPadding=20.0;
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:salah_construction/constants.dart';
 import 'package:salah_construction/screens/home/home_screen.dart';
 
 void main() {
@@ -6,6 +7,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  MaterialColor buildMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    strengths.forEach((strength) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    });
+    return MaterialColor(color.value, swatch);
+  }
+
   const MyApp({Key? key}) : super(key: key);
 
   @override
@@ -13,7 +34,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Salah Construction',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: buildMaterialColor(salahSandColor),
       ),
       home: const HomeScreen(title: 'Salah Construction'),
     );
