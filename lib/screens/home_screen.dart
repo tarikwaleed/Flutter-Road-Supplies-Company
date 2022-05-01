7f9f3fa07e5f0c28372a81e907838c5c92c27812
takizee <takizee53@gmail.com>
2022-05-01 07:14:49 +0700

    :lipstick: Adding BottomNavigationBar and image in appBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:salah_construction/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
@@ -10,16 +11,63 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'الخلاطات ',
+      style: optionStyle,
+    ),
+    Text(
+      'العملاء ',
+      style: optionStyle,
+    ),
+    Text(
+      'الموظفين',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white70,
         flexibleSpace: Center(
           child: Image(
             image: AssetImage('assets/images/logo.png'),
             fit: BoxFit.contain,
           ),
         ),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/logo.png')),
+            label: ' الخلاطات'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money_sharp),
+            label: 'العملاء',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'الموظفين',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: salahConcreteColor,
+        unselectedItemColor: Colors.grey[500],
+        onTap: _onItemTapped,
       ),
     );
   }
