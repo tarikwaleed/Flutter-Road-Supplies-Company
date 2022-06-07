120234f1773e1476a672e76340a9a0bcb10fa8d1
takizee <takizee53@gmail.com>
2022-06-07 07:08:43 +0200

    :hammer: Factor out ShipmentListFuture in a separate class

--- lib/components/shipment_list_future.dart
+++ lib/components/shipment_list_future.dart
@@ -1,14 +1,123 @@
 import 'package:flutter/material.dart';
+
+import '../models/shipment_model.dart';
+import '../services/shipment_db_service.dart';
+
 class ShipmentListFuture extends StatefulWidget {
-  const ShipmentListFuture({Key? key}) : super(key: key);
+  final dynamic mixerData;
+
+  const ShipmentListFuture({Key? key, required this.mixerData})
+      : super(key: key);
 
   @override
   State<ShipmentListFuture> createState() => _ShipmentListFutureState();
 }
 
 class _ShipmentListFutureState extends State<ShipmentListFuture> {
+  ShipmentDBService shipmentDBService = ShipmentDBService();
+  Future<List<Shipment>>? shipmentsListFuture;
+  List<Shipment>? retrievedShipmentsList;
+
+  Future<void> _refresh() async {
+    shipmentsListFuture =
+        shipmentDBService.retrieveShipmentsByMixerId(widget.mixerData.id);
+    retrievedShipmentsList =
+        await shipmentDBService.retrieveShipmentsByMixerId(widget.mixerData.id);
+    setState(() {});
+  }
+
+  // void _dismiss() {
+  //   shipmentsListFuture =
+  //       shipmentDBService.retrieveShipmentsByMixerId(widget.mixerData.id);
+  // }
+
+  Future<void> _initRetrieval() async {
+    shipmentsListFuture =
+        shipmentDBService.retrieveShipmentsByMixerId(widget.mixerData.id);
+    retrievedShipmentsList =
+        await shipmentDBService.retrieveShipmentsByMixerId(widget.mixerData.id);
+
+    setState(() {});
+    debugPrint(
+        "retrieved shipment list is" + retrievedShipmentsList.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initRetrieval();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return RefreshIndicator(
+      onRefresh: _refresh,
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: FutureBuilder(
+          future: shipmentsListFuture,
+          builder:
+              (BuildContext context, AsyncSnapshot<List<Shipment>> snapshot) {
+            if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+              return ListView.separated(
+                  itemCount: retrievedShipmentsList!.length,
+                  separatorBuilder: (context, index) => const SizedBox(
+                        height: 10,
+                      ),
+                  itemBuilder: (_, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: ListTile(
+                        // onTap: () {
+                        //   Navigator.pushNamed(context, '/mixer_details',
+                        //       arguments: retrievedShipmentsList![index]);
+                        // },
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          side: BorderSide(color: Colors.blue),
+                        ),
+                        title: Text(
+                          "رقم العربية: " +
+                              retrievedShipmentsList![index]
+                                  .vehicleNumber
+                                  .toString(),
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                        subtitle: Text(
+                          "رقم المقطورة: " +
+                              retrievedShipmentsList![index]
+                                  .cartNumber
+                                  .toString(),
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        trailing: const Icon(Icons.arrow_right_sharp),
+                      ),
+                    );
+                  });
+            } else if (snapshot.connectionState == ConnectionState.done &&
+                retrievedShipmentsList!.isEmpty) {
+              return Center(
+                child: ListView(
+                  children: const <Widget>[
+                    Align(
+                        alignment: AlignmentDirectional.center,
+                        child: Text(
+                          'لا يوجد نقل',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                        )),
+                  ],
+                ),
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/screens/mixer_details.dart
+++ lib/screens/mixer_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:salah_construction/components/add_shipment_popup.dart';
+import 'package:salah_construction/components/shipment_list_future.dart';
 import 'package:salah_construction/models/shipment_model.dart';
 import '../services/shipment_db_service.dart';
 
@@ -14,40 +15,6 @@ class MixerDetailsScreen extends StatefulWidget {
 }
 
 class _MixerDetailsScreenState extends State<MixerDetailsScreen> {
-  ShipmentDBService shipmentDBService = ShipmentDBService();
-  Future<List<Shipment>>? shipmentsListFuture;
-  List<Shipment>? retrievedShipmentsList;
-
-  Future<void> _refresh() async {
-    shipmentsListFuture =
-        shipmentDBService.retrieveShipmentsByMixerId(widget.mixerData.id);
-    retrievedShipmentsList =
-        await shipmentDBService.retrieveShipmentsByMixerId(widget.mixerData.id);
-    setState(() {});
-  }
-
-  // void _dismiss() {
-  //   shipmentsListFuture =
-  //       shipmentDBService.retrieveShipmentsByMixerId(widget.mixerData.id);
-  // }
-
-  Future<void> _initRetrieval() async {
-    shipmentsListFuture =
-        shipmentDBService.retrieveShipmentsByMixerId(widget.mixerData.id);
-    retrievedShipmentsList =
-        await shipmentDBService.retrieveShipmentsByMixerId(widget.mixerData.id);
-
-    setState(() {});
-    debugPrint(
-        "retrieved shipment list is" + retrievedShipmentsList.toString());
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _initRetrieval();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,75 +28,7 @@ class _MixerDetailsScreenState extends State<MixerDetailsScreen> {
           color: Colors.black,
         ),
       ),
-      body: RefreshIndicator(
-        onRefresh: _refresh,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: FutureBuilder(
-            future: shipmentsListFuture,
-            builder:
-                (BuildContext context, AsyncSnapshot<List<Shipment>> snapshot) {
-              if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-                return ListView.separated(
-                    itemCount: retrievedShipmentsList!.length,
-                    separatorBuilder: (context, index) => const SizedBox(
-                          height: 10,
-                        ),
-                    itemBuilder: (_, index) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16.0),
-                        ),
-                        child: ListTile(
-                          // onTap: () {
-                          //   Navigator.pushNamed(context, '/mixer_details',
-                          //       arguments: retrievedShipmentsList![index]);
-                          // },
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                            side: BorderSide(color: Colors.blue),
-                          ),
-                          title: Text(
-                            "رقم العربية: " +
-                                retrievedShipmentsList![index]
-                                    .vehicleNumber
-                                    .toString(),
-                            style: Theme.of(context).textTheme.bodyText1,
-                          ),
-                          subtitle: Text(
-                            "رقم المقطورة: " +
-                                retrievedShipmentsList![index]
-                                    .cartNumber
-                                    .toString(),
-                            style: Theme.of(context).textTheme.bodyText2,
-                          ),
-                          trailing: const Icon(Icons.arrow_right_sharp),
-                        ),
-                      );
-                    });
-              } else if (snapshot.connectionState == ConnectionState.done &&
-                  retrievedShipmentsList!.isEmpty) {
-                return Center(
-                  child: ListView(
-                    children: const <Widget>[
-                      Align(
-                          alignment: AlignmentDirectional.center,
-                          child: Text(
-                            'لا يوجد نقل',
-                            style: TextStyle(
-                              color: Colors.red,
-                            ),
-                          )),
-                    ],
-                  ),
-                );
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
-        ),
-      ),
+      body: ShipmentListFuture(mixerData: widget.mixerData),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           AddShipmentPopup().showAddShipmentPopup(context);
