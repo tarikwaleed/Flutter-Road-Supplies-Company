f9a71953fb89c56ce223c4edcff43cd6cc558ba4
takizee <takizee53@gmail.com>
2022-09-07 17:49:53 +0200

    :construction

--- lib/viewmodels/shipment_item_viewmodel.dart
+++ lib/viewmodels/shipment_item_viewmodel.dart
@@ -6,15 +6,99 @@ import 'package:salah_construction/dtos/dtos.dart';
 
 class ShipmentItemViewmodel extends ChangeNotifier {
   Shipment _shipment = Shipment.empty();
-  String mixerId = '';
-  num carriagePrice = 0;
-  String cartNumber = '';
-  String vehicleNumber = '';
-  Timestamp date = Timestamp.fromDate(DateTime.now());
-  String materialId = '';
-  num materialPrice = 0;
-  String sourceId = '';
-  num totalPrice = 0;
-  num volume = 0;
-  String clientId = '';
+  String _mixerId = '';
+  num _carriagePrice = 0;
+  String _cartNumber = '';
+  String _vehicleNumber = '';
+  Timestamp _date = Timestamp.fromDate(DateTime.now());
+  String _materialId = '';
+  num _materialPrice = 0;
+  String _sourceId = '';
+  num _totalPrice = 0;
+  num _volume = 0;
+  String _clientId = '';
+
+  Shipment get shipment => _shipment;
+
+  set shipment(Shipment value) {
+    _shipment = value;
+    notifyListeners();
+  }
+
+  String get mixerId => _mixerId;
+
+  set mixerId(String value) {
+    _mixerId = value;
+    notifyListeners();
+  }
+
+  num get carriagePrice => _carriagePrice;
+
+  set carriagePrice(num value) {
+    _carriagePrice = value;
+    notifyListeners();
+  }
+
+  String get cartNumber => _cartNumber;
+
+  set cartNumber(String value) {
+    _cartNumber = value;
+    notifyListeners();
+  }
+
+  String get vehicleNumber => _vehicleNumber;
+
+  set vehicleNumber(String value) {
+    _vehicleNumber = value;
+    notifyListeners();
+  }
+
+  Timestamp get date => _date;
+
+  set date(Timestamp value) {
+    _date = value;
+    notifyListeners();
+  }
+
+  String get materialId => _materialId;
+
+  set materialId(String value) {
+    _materialId = value;
+    notifyListeners();
+  }
+
+  num get materialPrice => _materialPrice;
+
+  set materialPrice(num value) {
+    _materialPrice = value;
+    notifyListeners();
+  }
+
+  String get sourceId => _sourceId;
+
+  set sourceId(String value) {
+    _sourceId = value;
+    notifyListeners();
+  }
+
+  num get totalPrice => _totalPrice;
+
+  set totalPrice(num value) {
+    _totalPrice = value;
+    notifyListeners();
+  }
+
+  num get volume => _volume;
+
+  set volume(num value) {
+    _volume = value;
+    notifyListeners();
+  }
+
+  String get clientId => _clientId;
+
+  set clientId(String value) {
+    _clientId = value;
+    notifyListeners();
+  }
 }
