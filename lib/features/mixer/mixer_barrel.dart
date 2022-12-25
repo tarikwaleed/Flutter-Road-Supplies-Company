0646cacf1f9900a333e87e3b0e7411d02af10a5f
tarikwaleed <takizee53@gmail.com>
2022-12-25 20:03:56 +0200

    :construction:

---
+++ lib/features/mixer/mixer_barrel.dart
@@ -0,0 +1,9 @@
+//GENERATED BARREL FILE 
+export 'all_mixers_screen.dart'; 
+export 'mixer_card.dart'; 
+export 'mixer_card_view_model.dart'; 
+export 'mixer_data_repository.dart'; 
+export 'mixer_dto.dart'; 
+export 'mixer_firestore_service.dart'; 
+export 'mixers_data_bucket.dart'; 
+export 'mixers_list.dart'; 
